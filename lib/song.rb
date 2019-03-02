5d0b6dce79bfb00689d168152a8d859dcfecf2df
veslie losius <veslielosius@yahoo.com>
2019-03-02 00:06:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+ end 
+end 
+class Song 
+  @@song_count = 0 
+ 
+  end
+
+  
+  
+
+  
+   
+  
